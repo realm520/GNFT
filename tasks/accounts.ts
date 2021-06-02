c1dfecfd1aa2dac83cb89488877ad9338bf86ae2
harryub <harryub@ub.com>
2021-06-02 20:38:47 +0800

    feat: extend accounts command

--- tasks/accounts.ts
+++ tasks/accounts.ts
@@ -1,12 +1,14 @@
+import "@nomiclabs/hardhat-web3";
 import { Signer } from "@ethersproject/abstract-signer";
 import { task } from "hardhat/config";
 
 import { TASK_ACCOUNTS } from "./task-names";
 
 task(TASK_ACCOUNTS, "Prints the list of accounts", async (_taskArgs, hre) => {
-  const accounts: Signer[] = await hre.ethers.getSigners();
+  const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(await account.getAddress());
+    const balance = await hre.web3.eth.getBalance(await account.getAddress());
+    console.log(await account.getAddress(), "\t", hre.web3.utils.fromWei(balance, "ether"), "ETH");
   }
 });
